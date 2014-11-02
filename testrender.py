76165b2bf4abf668c49cc2e5a22addf0af15bae5
Amelius15 <anthonylobko@gmail.com>
2014-11-01 20:21:37 -0400

    take a look

--- testrender.py
+++ testrender.py
@@ -4,11 +4,12 @@ g = []
 bl  = []
 for i in xrange(3):
   d = display(x = 0+i*520, y = 0, width=520, height=1080)
+  d.forward= (0,0,5)
   g.append(d)
 texdat = materials.loadTGA("testtext")
 tex = materials.texture(data = texdat, mapping = "rectangular", interpolate = False)
 for i in xrange(3):
-  redbox=box(pos=vector(0,-2,0),
+  redbox=box(pos=vector(0,0,0),
            size=(2,2,2),display = g[i], color=color.green, material = tex)
   print g[i].forward
 
@@ -18,5 +19,34 @@ def keyInput(evt):
             print "Display #" + str(i)
             print g[i].forward
             print g[i].fov
+def rotInput(evt):
+    s = evt.key
+    if s=='a':
+        g[0].forward = g[0].forward.rotate(angle = -.1, axis = (0,0,1))
+        g[0].center = 5*norm(g[0].forward)
+        
+    elif s=='s':
+        g[0].forward = g[0].forward.rotate(angle = .1, axis = (0,0,1))
+        g[0].center = 5*norm(g[0].forward)
+        
+    elif s=='d':
+        g[1].forward = g[1].forward.rotate(angle = -.1, axis = (0,0,1))
+        g[1].center = 5*norm(g[1].forward)
+
+    elif s=='f':
+        g[1].forward = g[1].forward.rotate(angle = .1, axis = (0,0,1))
+        g[1].center = 5*norm(g[1].forward)
+
+    elif s=='g':
+        g[2].forward = g[2].forward.rotate(angle = -.1, axis = (0,0,1))
+        g[2].center = 5*norm(g[2].forward)
+
+    elif s == 'h':
+        g[2].forward = g[2].forward.rotate(angle = .1, axis = (0,0,1))
+        g[2].center = 5*norm(g[2].forward)
+        
+
 for i in xrange(3):
-    g[i].bind('click', keyInput)
+        g[i].bind('click', keyInput)
+        g[i].bind('keydown', rotInput)
+    
