c41a0ff98a2069271cd3efed47ce72c22882806f
maye-vaca <madeleyne.vaca@gmail.com>
2014-11-02 04:28:35 -0500

    updated graph file

--- graphAndInput.py
+++ graphAndInput.py
@@ -3,9 +3,27 @@ import sympy
 from sympy import *
 import ttk
 from Tkinter import *
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
+from mpl_toolkits.mplot3d import Axes3D
 
 def show_formula():
-    print("%s", formula.get())
+    print(formula.get())
+    Str_equation = formula.get()
+    eq = sympify(Str_equation)
+    tup = []
+    for i in xrange (0, 10):
+        temp = []
+        for j in xrange(0,10):
+            eq2 = eq.subs(x,i)
+            eq3 = eq2.subs(y,j)
+            temp.append ((i, j, eq3.evalf()))
+        tup.append(temp)
+    for lists in tup:
+        c= curve(pos = lists, radius=0.05)
+        
+    
 
 master = Tk()
 Label(master, text="Enter Formula Here").grid(row = 0)
@@ -18,3 +36,4 @@ def main():
     outside = box(color = color.cyan, opacity = 0.0, pos = (0,0,0))
     
     
+mainloop()
